8a67ebb82737f839bf2b7b8bd923f4ce3783574e
rabdelaziz <rabdelaziz>
2016-06-29 08:45:51 +0200

    commit tt

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,9 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+
+
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
