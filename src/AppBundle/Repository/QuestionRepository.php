e031af3041add64a87b6bf5bc36fcf96647c3745
saliou <saliu.diallo@gmail.com>
2017-05-07 18:33:22 +0200

    [EVO] Génération et MAJ des Repositories

--- src/AppBundle/Repository/QuestionRepository.php
+++ src/AppBundle/Repository/QuestionRepository.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Repository;
 
+use Doctrine\ORM\Mapping\OrderBy;
+
 /**
  * QuestionRepository
  *
@@ -10,4 +12,46 @@ namespace AppBundle\Repository;
  */
 class QuestionRepository extends \Doctrine\ORM\EntityRepository
 {
+	
+	public function getQuestionsByTopicIdAndLevel($topicId, $questionLevel, $limit = null, $offset = null)
+	{
+		$qb = $this->createQueryBuilder('q')
+			->leftJoin('q.topic', 't')
+			->leftJoin('q.level', 'l')
+			->where('topic_id = :topicId')
+			->setParameter('topicId', $topicId)
+			->andWhere('l.name = :name')
+			->setParameter('name', $questionLevel);
+		
+		if (isset($limit)) {
+			$qb->setMaxResults($limit);
+		}
+		if (isset($offset)) {
+			$qb->setFirstResult($offset);
+		}
+		
+		return $qb->getQuery()->getResult();
+	}
+	
+	public function getQuestionsWithTopic($topicName, $questionLevel, $limit = null, $offset = null, $orderBy = 'DESC')
+	{
+		$qb = $this->createQueryBuilder('q')
+			->leftJoin('q.topic', 't')
+			->leftJoin('q.level', 'l')
+			->where('t.name = :topicName')
+			->setParameter('topicName', $topicName)
+			->andWhere('l.name = :levelName')
+			->setParameter('levelName', $questionLevel)
+			->orderBy('q.id', $orderBy);
+		
+		if (isset($limit)) {
+			$qb->setMaxResults($limit);
+		}
+		if (isset($offset)) {
+			$qb->setFirstResult($offset);
+		}
+	
+		return $qb->getQuery()->getResult();
+	}
+	
 }
