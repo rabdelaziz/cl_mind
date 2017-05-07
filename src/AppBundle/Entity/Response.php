9dafb45e914055ba1bc6791bc4cf8ae4f31a61f8
saliou <saliu.diallo@gmail.com>
2017-05-07 17:36:38 +0200

    [REFACTOR] Renommage des classes Reponse et Niveau pour mettre en englais

---
+++ src/AppBundle/Entity/Response.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Response
+ *
+ * @ORM\Table(name="response")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ResponseRepository")
+ */
+class Response
+{
+	/**
+	 * @var int
+	 *
+	 * @ORM\Column(name="id", type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="wording", type="text", length=500, unique=false)
+	 */
+	private $wording;
+
+	/**
+	 * @var bool
+	 *
+	 * @ORM\Column(name="correct", type="boolean", options={"default":0})
+	 */
+	private $correct ;
+
+	/**
+	 * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Question", inversedBy="responses", cascade={"persist"})
+	 * @ORM\JoinColumn(nullable=false)
+	 */
+	private $question;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set wording
+     *
+     * @param string $wording
+     *
+     * @return Response
+     */
+    public function setWording($wording)
+    {
+        $this->wording = $wording;
+
+        return $this;
+    }
+
+    /**
+     * Get wording
+     *
+     * @return string
+     */
+    public function getWording()
+    {
+        return $this->wording;
+    }
+
+    /**
+     * Set correct
+     *
+     * @param boolean $correct
+     *
+     * @return Response
+     */
+    public function setCorrect($correct)
+    {
+        $this->correct = $correct;
+
+        return $this;
+    }
+
+    /**
+     * Get correct
+     *
+     * @return boolean
+     */
+    public function getCorrect()
+    {
+        return $this->correct;
+    }
+
+    /**
+     * Set question
+     *
+     * @param \AppBundle\Entity\Question $question
+     *
+     * @return Response
+     */
+    public function setQuestion(\AppBundle\Entity\Question $question)
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    /**
+     * Get question
+     *
+     * @return \AppBundle\Entity\Question
+     */
+    public function getQuestion()
+    {
+        return $this->question;
+    }
+}
